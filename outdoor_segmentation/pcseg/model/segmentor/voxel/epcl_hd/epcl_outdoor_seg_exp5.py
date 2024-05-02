826533791cca5cb8803e37c6daa7bf5fe54557bc
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-02 10:43:57 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -159,7 +159,6 @@ class HD_model():
         hv_2 = self.random_projection_2(input_x[2])
 
         hv_all = torchhd.bundle(torchhd.bundle(hv_0, hv_1),hv_2).sign()
-        print(hv_all.shape)
 
         return hv_all
     
