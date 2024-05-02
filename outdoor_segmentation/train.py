71508a77d9c231b9e115562c9839349d99041faf
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-02 07:06:20 -0700

    Debug

--- outdoor_segmentation/train.py
+++ outdoor_segmentation/train.py
@@ -328,6 +328,7 @@ class Trainer:
         self.optimizer.load_state_dict(checkpoint['optimizer_state'])
         self.scaler.load_state_dict(checkpoint['scaler_state'])
         self.scheduler.load_state_dict(checkpoint['scheduler_state'])
+        print("train HD?", not self.args.train_hd)
         if not self.args.train_hd:
             file = os.listdir(self.ckp_dir)[-1]
             checkpoint_hd = torch.load(file)
@@ -438,7 +439,7 @@ class Trainer:
         metric = {}
         metric['hist_list'] = []
 
-        print(self.model.hd_model.classes_hv)
+        print("INIT \n", self.model.hd_model.classes_hv)
 
         for i, batch_dict in enumerate(dataloader):
             load_data_to_gpu(batch_dict)
