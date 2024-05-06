52f1015aa1e20a2f405487a92cf117faa15057f1
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-05 20:26:09 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -202,15 +202,15 @@ class HD_model():
         self.random_projection = self.random_projection.to(*args)
 
     def encode(self, input_x):
-        print(input_x.shape)
+        #print(input_x.shape)
         hv_0 = self.random_projection(input_x)
-        print(hv_0.shape) # (3,#,d)
+        #print(hv_0.shape) # (3,#,d)
         #hv_1 = self.random_projection[1](input_x[1])
         #hv_2 = self.random_projection[2](input_x[2])
         #hv_012 = torch.stack((hv_0, hv_1, hv_2))
         hv_all = torch.sum(hv_0, dim=0).sign()
 
-        x = input("Enter")
+        #x = input("Enter")
 
         return hv_all
     
