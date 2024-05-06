42c7160c3ba46b033e7c18ba68db3b7b828b3bb1
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-05 21:38:33 -0700

    Early stop testing

--- outdoor_segmentation/pcseg/model/segmentor/__init__.py
+++ outdoor_segmentation/pcseg/model/segmentor/__init__.py
@@ -56,7 +56,7 @@ def build_segmentor(model_cfgs, num_class, exp=1):
     elif exp==4:
         from .voxel.epcl_hd.epcl_outdoor_seg_exp4 import EPCLOutdoorSegHD 
     elif exp==5:
-        from .voxel.epcl_hd.epcl_outdoor_seg_exp5 import EPCLOutdoorSegHD
+        from .voxel.epcl_hd.epcl_outdoor_seg_exp5_crop import EPCLOutdoorSegHD
     elif exp==6:
         from .voxel.epcl_hd.epcl_outdoor_seg_exp6 import EPCLOutdoorSegHD  
 
