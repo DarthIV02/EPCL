19de3c9ef6972d4d2f6149dab5df3405f3796875
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-05 23:33:34 -0700

    Update epcl_outdoor_seg_exp5_crop.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
@@ -580,7 +580,7 @@ class EPCLOutdoorSegHD(BaseSegmentor):
         #print(sim.shape)
         sim_arg = torch.argmax(sim, dim=1)
         sim = torch.max(sim, dim=1).values
-        mask_sim = sim < 0.075
+        mask_sim = sim < 0.070
         print("Skipped: ", torch.sum(~mask_sim))
         print("max y_2: ", torch.max(sim))
         print("mean y_2: ", torch.mean(sim))
