2b0a635cc5094ab1f1f8fb3477cdfebd47b6622a
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-12 17:18:35 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -241,8 +241,10 @@ class HD_model():
             idx = idx.to(self.device)
             class_batch = classification[idx].type(torch.LongTensor).to(self.device)
             
-            print(sim_all[:, class_batch].shape)
+            #print(sim_all[:, class_batch].shape)
             novelty = 1 - sim_all[:, class_batch]
+            print(hv_all.shape)
+            print("Novelty", novelty.shape)
             updates = hv_all.transpose(0,1)*torch.mul(novelty, self.lr)
             updates = updates.transpose(0,1)
             
