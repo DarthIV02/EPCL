0eeb208dca4dcf98abbfaa8e35e3225228dcabb9
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-13 19:40:49 -0700

    Uodate with pad

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
@@ -190,8 +190,8 @@ class HD_model():
         self.flatten = nn.Flatten(0,1)
         self.softmax = torch.nn.Softmax(dim=1)
         self.random_projection_0 = torchhd.embeddings.Projection(num_features[0], self.d, device=kwargs['device'])
-        self.random_projection_1 = torchhd.embeddings.Projection(num_features[1], self.d, device=kwargs['device'])
-        self.random_projection_2 = torchhd.embeddings.Projection(num_features[2], self.d, device=kwargs['device'])
+        self.random_projection_1 = torchhd.embeddings.Projection(num_features[0], self.d, device=kwargs['device'])
+        self.random_projection_2 = torchhd.embeddings.Projection(num_features[0], self.d, device=kwargs['device'])
         #self.random_projection = {0:self.random_projection_0, 1:self.random_projection_1, 2:self.random_projection_2,}
         #self.random_projection = (self.random_projection_0, self.random_projection_1, self.random_projection_2)
         #self.random_projection = BatchProjection(3, num_features[0], self.d, device=kwargs['device'])
