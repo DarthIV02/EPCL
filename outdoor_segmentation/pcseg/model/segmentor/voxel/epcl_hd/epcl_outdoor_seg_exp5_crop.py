ace65fa043c3c3931d82e2b26d6a4fcb5cc41053
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-14 03:46:42 -0700

    Reshepe forward

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5_crop.py
@@ -239,14 +239,12 @@ class HD_model():
         return sim
     
     def train(self, input_points, classification, **kwargs):
-        hv_all, sim_all, pred_labels = self.forward(input_points)
-        classification = classification.type(torch.LongTensor).to(self.device)
         for idx in torch.arange(input_points[0].shape[0]).chunk(self.div):
+            hv_all, sim_all, pred_labels = self.forward(input_points[:, idx, :])
             idx = idx.to(self.device)
-            class_batch = classification[idx]
-
-            novelty = 1 - sim_all[idx, class_batch]
-            updates = hv_all[idx].transpose(0,1)*torch.mul(novelty, self.lr)
+            class_batch = classification[idx].type(torch.LongTensor).to(self.device)
+            novelty = 1 - sim_all[torch.arange(idx.shape[0]), class_batch]
+            updates = hv_all.transpose(0,1)*torch.mul(novelty, self.lr) # Normal HD with novelty
             updates = updates.transpose(0,1)
             
             # Update all of the classes with the actual label
@@ -257,16 +255,17 @@ class HD_model():
             #if (pred_labels != c):
             #    self.classes_hv[pred_labels] += -1*hv_all*self.lr*(1-sim_all[pred_labels])
             
-            mask_dif = class_batch != pred_labels[idx]
             
-            novelty = 1 - sim_all[idx[mask_dif], pred_labels[idx][mask_dif]] # only the ones updated
-            updates = hv_all[idx][mask_dif].transpose(0,1)*torch.mul(novelty, self.lr)
+            # ONLINEHD
+            mask_dif = class_batch != pred_labels
+            novelty = 1 - sim_all[mask_dif, pred_labels[mask_dif]] # only the ones updated
+            updates = hv_all[mask_dif].transpose(0,1)*torch.mul(novelty, self.lr)
             updates = torch.mul(updates, -1)
             updates = updates.transpose(0,1)
             updates_2 = torch.zeros((idx.shape[0], self.d), device=self.device) # all zeros original
             updates_2[mask_dif] = updates # update vectors for the ones that changed
 
-            self.classes_hv.index_add_(0, pred_labels[idx], updates_2)
+            self.classes_hv.index_add_(0, pred_labels, updates_2)
 
 class Bottleneck(nn.Module):
     expansion = 4
