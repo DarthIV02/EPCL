510592960e66ca7cbc184a1907ba492c1b1a4c8b
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-14 00:51:14 -0700

    Update val.sh

--- outdoor_segmentation/scripts/val.sh
+++ outdoor_segmentation/scripts/val.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 NGPUS=1
 cfg_name=EPCL_HD
 cfg_file=tools/cfgs/voxel/nuscenes/${cfg_name}.yaml
-extra_tag=val_${cfg_name}_nuscenes_x5
+extra_tag=val_${cfg_name}_nuscenes
 pretrained_model=/root/main/EPCL_setup/checkpoints/best_checkpoint.pth
 
 set -x
