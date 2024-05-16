4b80eb25254918194e3216cb3f79c46dc6747948
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-15 22:11:43 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -195,8 +195,8 @@ class HD_model():
         #self.random_projection_2 = torchhd.embeddings.Projection(num_features[2], self.d, device=kwargs['device'])
         #self.random_projection = {0:self.random_projection_0, 1:self.random_projection_1, 2:self.random_projection_2,}
         #self.random_projection = self.random_projection_0, self.random_projection_1, self.random_projection_2)
-        self.random_projection = BatchProjection(2, num_features[0], self.d, device=kwargs['device'])
-        self.stages = torchhd.random(2, d, device=kwargs['device'])
+        self.random_projection = BatchProjection(3, num_features[0], self.d, device=kwargs['device'])
+        self.stages = torchhd.random(3, d, device=kwargs['device'])
         #self.random_projection_global = torchhd.embeddings.Projection(num_features, self.d)
         self.lr = lr
         self.bicycle = None
