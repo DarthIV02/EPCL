24056952e1c3c81f13124b0e549341360599be24
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-19 18:15:22 -0700

    Remove bad and smaller lr

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -304,18 +304,18 @@ class HD_model():
             
             
             # ONLINEHD
-            #mask_dif = class_batch != pred_labels
-            #novelty = 1 - sim_all[mask_dif, pred_labels[mask_dif]] # only the ones updated
-            #updates = hv_all[mask_dif].transpose(0,1)*torch.mul(novelty, self.lr)
-            #updates = torch.mul(updates, -1)
-            #updates = updates.transpose(0,1)
-            #updates_2 = torch.zeros((idx.shape[0], self.d), device=self.device) # all zeros original
-            #updates_2[mask_dif] = updates # update vectors for the ones that changed
+            mask_dif = class_batch != pred_labels
+            novelty = 1 - sim_all[mask_dif, pred_labels[mask_dif]] # only the ones updated
+            updates = hv_all[mask_dif].transpose(0,1)*torch.mul(novelty, self.lr)
+            updates = torch.mul(updates, -1)
+            updates = updates.transpose(0,1)
+            updates_2 = torch.zeros((idx.shape[0], self.d), device=self.device) # all zeros original
+            updates_2[mask_dif] = updates # update vectors for the ones that changed
 
-            #self.classes_hv.index_add_(0, pred_labels, updates_2)
+            self.classes_hv.index_add_(0, pred_labels, updates_2)
 
             # Update positive when different 
-            mask_dif = class_batch != pred_labels
+            #mask_dif = class_batch != pred_labels
             novelty = 1 - sim_all[mask_dif, class_batch[mask_dif]] # only the ones updated
             updates = hv_all[mask_dif].transpose(0,1)*torch.mul(novelty, self.lr)
             #updates = torch.mul(updates, -1)
