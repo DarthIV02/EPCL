2bcc4d616f24c6f5c42b67526bde9afb3c315872
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-23 22:52:13 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -286,10 +286,10 @@ class HD_model():
         input_points = input_points[true_val]
         classification = classification[true_val]
         self.num_samples_per_class = self.num_samples_per_class + torch.bincount(classification)
-        x = torch.mul(self.num_samples_per_class, self.num_classes)
+        x = torch.mul(self.num_samples_per_class, self.num_classes) + 1e-8
         print(x)
         print(x.shape)
-        weight_for_class_i = torch.div(torch.sum(self.num_samples_per_class), (x + 1e-8))
+        weight_for_class_i = torch.div(torch.sum(self.num_samples_per_class), x)
         #coords = kwargs['batch_dict']['lidar'].C[true_val]
 
         #coords, not_outlier, input_points, classification = self.clean_z(kwargs['batch_dict']['lidar'].C[true_val], input_points, classification)
