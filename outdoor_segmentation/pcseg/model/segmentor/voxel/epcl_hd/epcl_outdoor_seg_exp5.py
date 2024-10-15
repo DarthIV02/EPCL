01371fe1a011003b1ab9771c2e10010acf2bd8ac
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-14 20:29:25 -0700

    Update epcl_outdoor_seg_exp5.py

--- outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
+++ outdoor_segmentation/pcseg/model/segmentor/voxel/epcl_hd/epcl_outdoor_seg_exp5.py
@@ -220,7 +220,6 @@ class HD_model():
         #print(coords.shape)
         input_x = input_x.transpose(0,1)
         hv_0 = self.random_projection(input_x)
-        del input_x
         hv_0 = hv_0.transpose(0,1)
         #print(hv_0.shape)
         #hv_0 = torch.cat((hv_0, coords), dim=1) <----
@@ -229,6 +228,7 @@ class HD_model():
         #hv_0 = torch.cos(hv_0 + self.bias) * torch.sin(hv_0)
         hv_0 = hv_0.sign() # <-- BATCH
         repeated = self.stages[:3].repeat(input_x.shape[1], 1, 1)
+        del input_x
         hv_0 = torchhd.bind(hv_0, repeated)
         del repeated
 
