2522f5385148998a4afafbad082747d09790e3ca
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-22 04:00:39 -0700

    Update laserscanvis.py

--- outdoor_segmentation/auxiliary/laserscanvis.py
+++ outdoor_segmentation/auxiliary/laserscanvis.py
@@ -60,6 +60,9 @@ class LaserScanVis:
     with torch.no_grad():
         ret_dict = inference_model(first)
     pc, labels, pred = first['lidar'].C.float(), ret_dict['point_labels'], ret_dict['point_predict']
+    print(pc.shape)
+    print(labels.shape)
+    print(pred.shape)
     if isinstance(pred, torch.Tensor):
       if pred.size() != labels.size():
           pred = nn.functional.softmax(pred, dim=1).argmax(dim=1)
