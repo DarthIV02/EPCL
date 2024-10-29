4cbeec1727efe7f3225eb5eac568c91d7105c7f4
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-28 22:36:28 -0700

    Change to mini kitti

--- outdoor_segmentation/pcseg/data/dataset/semantickitti/semantickitti.py
+++ outdoor_segmentation/pcseg/data/dataset/semantickitti/semantickitti.py
@@ -48,13 +48,13 @@ class SemantickittiDataset(data.Dataset):
             self.split = 'test'
 
         if self.split == 'train':
-            self.seqs = ['00', '01', '02', '03', '04', '05', '06', '07', '09', '10']
+            self.seqs = ['00'] # '01', '02', '03', '04', '05', '06', '07', '09', '10'
         elif self.split == 'val':
-            self.seqs = ['08']
+            self.seqs = ['01']
         elif self.split == 'train_val':
-            self.seqs = ['00', '01', '02', '03', '04', '05', '06', '07', '09', '10', '08']
+            self.seqs = ['00', '01'] # , '02', '03', '04', '05', '06', '07', '09', '10', '08'
         elif self.split == 'test':
-            self.seqs = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21']
+            self.seqs = ['02'] # , '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'
         else:
             raise Exception('split must be train/val/train_val/test.')
         
