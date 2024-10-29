a37462cc9983844b22100cb993910bd4710911a7
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-28 23:00:56 -0700

    Update train.py

--- outdoor_segmentation/train.py
+++ outdoor_segmentation/train.py
@@ -288,7 +288,7 @@ class Trainer:
         if args.fix_random_seed:
             common_utils.set_random_seed(42)
 
-        log_dir = cfgs.ROOT_DIR / 'logs' / cfgs.EXP_GROUP_PATH[5:] / cfgs.TAG / args.extra_tag
+        log_dir = '.' / 'logs' / cfgs.EXP_GROUP_PATH[5:] / cfgs.TAG / args.extra_tag
         ckp_dir = log_dir / 'ckp'
         print("Log Dir", log_dir)
         log_dir.mkdir(parents=True, exist_ok=True)
