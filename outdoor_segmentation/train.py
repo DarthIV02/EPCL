69ee2e0a41d7155adfc07b7fc467bf36fff087cd
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-28 23:12:20 -0700

    Update train.py

--- outdoor_segmentation/train.py
+++ outdoor_segmentation/train.py
@@ -289,9 +289,9 @@ class Trainer:
             common_utils.set_random_seed(42)
 
         partial = os.path.join(os.getcwd(), 'logs')
-        print(partial)
+        partial = partial + cfgs.EXP_GROUP_PATH[5:]
 
-        log_dir = os.path.join(partial, cfgs.EXP_GROUP_PATH[5:]) # , cfgs.TAG, args.extra_tag
+        log_dir = os.path.join(partial, cfgs.TAG, args.extra_tag) # , 
         ckp_dir = os.path.join(log_dir, 'ckp')
         print("Log Dir", log_dir)
         x = input()
