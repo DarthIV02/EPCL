1f0b248e234d15ffe19177771dd18c42c8b3c922
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-11-02 15:56:50 -0700

    Update val.sh

--- outdoor_segmentation/scripts/val.sh
+++ outdoor_segmentation/scripts/val.sh
@@ -12,7 +12,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 NGPUS=1
 cfg_name=EPCL_HD
 cfg_file=tools/cfgs/voxel/semantic_kitti/${cfg_name}.yaml
-extra_tag=val_${cfg_name}_kitti_x5
+extra_tag=val_${cfg_name}_semkitti
 pretrained_model=/root/main/EPCL_setup/checkpoints/best_checkpoint.pth
 
 #NGPUS=1
